411261d19d3fdf00e9f41f31c9325e3082c4f0de
Jordi Casanella <jordikiter@hotmail.es>
2023-02-02 00:22:45 +0000

    Fix error in import in controller

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,7 +1,6 @@
 import { Body, Controller, Get, Post } from '@nestjs/common';
 import { TasksService } from './tasks.service';
 import { Task } from './task.model';
-import { title } from 'process';
 
 @Controller('tasks')
 export class TasksController {
