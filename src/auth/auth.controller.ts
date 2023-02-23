af60558bd3f8cb9e1a902f3ce14a40cf41ae3c99
Jordi Casanella <jordikiter@hotmail.es>
2023-02-23 00:21:46 +0000

    Drop not required import

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,5 +1,4 @@
 import { Body, Controller, Post, Req, UseGuards } from '@nestjs/common';
-import { AuthGuard } from '@nestjs/passport';
 import { AuthService } from './auth.service';
 import { AuthCredentialsDto } from './dto/auth-credentials.dto';
 
