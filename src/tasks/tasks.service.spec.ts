2e98e7d330354a1921d7bd4799afe6c5f9d95103
jcasanella <jordikiter@hotmail.es>
2023-03-21 00:16:13 +0000

    First auth test

---
+++ src/tasks/tasks.service.spec.ts
@@ -0,0 +1,43 @@
+import { Test } from '@nestjs/testing';
+import { TasksRepository } from './tasks.repository';
+import { TasksService } from './tasks.service';
+
+const mockTasksRepository = () => ({
+  getTasks: jest.fn(),
+});
+
+const mockUser = {
+  username: 'Ariel',
+  id: 'someId',
+  password: 'somePassword',
+  tasks: [],
+};
+
+describe('TasksService', () => {
+  let taskService: TasksService;
+  let tasksRepository;
+
+  beforeEach(async () => {
+    const module = await Test.createTestingModule({
+      providers: [
+        TasksService,
+        { provide: TasksRepository, useFactory: mockTasksRepository },
+      ],
+    }).compile();
+
+    taskService = module.get(TasksService);
+    tasksRepository = module.get(TasksRepository);
+  });
+
+  describe('getTasks', () => {
+    it('calls TasksRepository.getTasks and returns the result', async () => {
+      expect(tasksRepository.getTasks).not.toHaveBeenCalled();
+
+      // call taskService.getTasks, which should then call the repository's getTasks
+      tasksRepository.getTasks.mockResolvedValue('someValue');
+      const result = await taskService.getTasks(null, mockUser);
+      expect(tasksRepository.getTasks).toHaveBeenCalled();
+      expect(result).toEqual('someValue');
+    });
+  });
+});
